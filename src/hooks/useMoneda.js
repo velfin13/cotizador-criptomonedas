4e2da3497f137dff238525398bc8e95af915479f
Velfin Velasquez <vvelasquez2465@utm.edu.ec>
2020-12-13 11:41:35 -0500

    se agrego style componnent al hook moneda

--- src/hooks/useMoneda.js
+++ src/hooks/useMoneda.js
@@ -1,4 +1,25 @@
 import React, { Fragment, useState } from "react";
+import styled from "@emotion/styled";
+
+const Label = styled.label`
+  font-family: "Bebas Neue", cursive;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 2.4rem;
+  margin-top: 2rem;
+  display: block;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  display: block;
+  padding: 1rem;
+  --webkit-appearance: none;
+  border-radius: 10px;
+  border: none;
+  font-size: 1.2rem;
+`;
 
 export const useMoneda = (label, initialState = "", opciones) => {
   //state del hook monda
@@ -11,15 +32,15 @@ export const useMoneda = (label, initialState = "", opciones) => {
 
     return (
       <Fragment>
-        <label>{label}</label>
-        <select onChange={hanledChange} value={moneda}>
+        <Label>{label}</Label>
+        <Select onChange={hanledChange} value={moneda}>
           <option value="">--Selecionar--</option>
           {opciones.map((opcion) => (
             <option key={opcion.codigo} value={opcion.codigo}>
               {opcion.nombre}
             </option>
           ))}
-        </select>
+        </Select>
       </Fragment>
     );
   };
